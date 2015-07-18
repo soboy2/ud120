85a4f7c7e90ce2eefe4b9a57821a92a9e8e894a0
Sho <soboy2@users.noreply.github.com>
2015-07-18 05:53:41 -0400

    add svm and decision tree algorithms

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python
 
-""" 
+"""
     this is the code to accompany the Lesson 3 (decision tree) mini-project
 
     use an DT to identify emails from the Enron corpus by their authors
-    
+
     Sara has label 0
     Chris has label 1
 
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn import tree
+from sklearn.metrics import accuracy_score
 
 
 ### features_train and features_test are the features for the training
@@ -29,5 +31,16 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 #########################################################
+#### your code goes here
+clf = tree.DecisionTreeClassifier()
+#clf50 = tree.DecisionTreeClassifier(min_samples_split=50) #min_samples_split=50
+clf = clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+
+acc = accuracy_score(labels_test, pred)
+### be sure to compute the accuracy on the test set
 
 
+
+def submitAccuracies():
+  return {"acc":round(acc,3)}
--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python
 
-""" 
+"""
     this is the code to accompany the Lesson 2 (SVM) mini-project
 
     use an SVM to identify emails from the Enron corpus by their authors
-    
+
     Sara has label 0
     Chris has label 1
 
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 
 
 ### features_train and features_test are the features for the training
@@ -29,4 +31,17 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 
+clf = SVC(kernel='linear', C=1.0)
 
+t0 = time() #timining time to train
+clf.fit(features_train, labels_train)
+#SVC(C=1.0, kernel='linear', gamma=0.0)
+print("training time:", round(time()-t0, 3), "s")
+
+t1 = time() #timining time to predict
+pred = clf.predict(features_test)
+print("prediction time:", round(time()-t1, 3), "s")
+
+accuracy = accuracy_score(labels_test, pred)
+
+print("accuracy:", str(accuracy))
